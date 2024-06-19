eaf00df28b6aff6c5116dd99017698d90deb05f0
Ilanna Freire <ilannafreiret@hotmail.com>
2024-06-19 11:20:08 -0300

    T1: variáveis js

---
+++ nivel1/variaveis.js
@@ -0,0 +1,63 @@
+// Nomes válidos de variáveis
+
+let nomeDaVariavel
+let nome12345 
+let nome_nome$
+let NOME 
+let nome 
+let Nome 
+let nOme 
+
+
+/* Nomes inválidos de variáveis
+
+let 1nome - começcar dessa forma
+let @ - caracteres
+let let - não use palavras reservadas
+
+*/
+
+// pode ir atribuindo novos valores
+
+let testandoPrimeiraVariavel
+
+console.log(testandoPrimeiraVariavel)
+
+testandoPrimeiraVariavel = "Ilanna"
+
+console.log(testandoPrimeiraVariavel)
+
+testandoPrimeiraVariavel = 28
+
+console.log(testandoPrimeiraVariavel)
+
+testandoPrimeiraVariavel = "Julia"
+
+console.log(testandoPrimeiraVariavel)
+
+// pode criar a variável e já atribuir valores
+
+let meuTime = "SPFC"
+
+console.log(meuTime)
+
+// outra forma de usar variável - está em desuso
+
+var meuCurso = "Engenharia de Software"
+
+console.log(meuCurso)
+
+
+
+/* - amarzenamento de dados.
+   - duas formas de fazer.
+   - let: palavra reservada do js
+    indica criação de variável
+   - JS consegue diferenciar variáveis com nomes iguais
+   e escrita diferente pois identifica o que é minúsculo 
+   e maiusculo. 
+
+
+
+
+*/

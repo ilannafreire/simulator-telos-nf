acc2d7cafea670438df5c30f4b987c5b9586b135
Ilanna Freire <ilannafreiret@hotmail.com>
2024-06-19 01:31:15 -0300

    T1: JS client X server

---
+++ nivel1/inicio.js
@@ -0,0 +1,27 @@
+
+/* 
+JS- linguagem de programação de alto nível
+  - Conhecida por sua versatilidade
+  - Aplicação em diversas áreas de desenvolvimento de 
+  software.
+  - Multiplataforma - pode ser executada em uma variedade de ambiente e 
+  dispositivos.
+  - Ecossistema rico de ferramentas e bibliotecas - vasto ecossistema de ferramentas, bibliotecas e
+  frameworks que facilitam o desenvolvimento de uma ampla gama de aplicativos.
+  - Adoção generalizada - JS é umas das linguagens mais populares e amplamente adotadas do mundo.
+  - Versatilidade - Utilizado tanto ao lado do cliente quanto do servidor.
+  
+JavaScript client
+  - JS do lado do cliente é executado no navegador web do usuário.
+  - Usado em páginas da web interativas e dinâmicas, respondendo a eventos
+  do usuário, manipulando o conteúdo da página e realizando validações de entrada.
+  - É utilizado para adicionar comportamento interativo a páginas da web como validações de formuçários,
+  animações e muito mais. 
+JavaScript server 
+   - JS do lado do servidor é executado no servidor em vez do navegador do usuário
+   - Usado para criar aplicativos web completos. 
+   - APIs RESTful, serviços web e outras aplicações de back-end
+   - Utilizado para lidar com lógica de negócios, acesso aos bancos de dados, autenticação de usuários, 
+   geraçãon de páginas dinâmicas e processamento de requisições HTTP.
+   
+*/
